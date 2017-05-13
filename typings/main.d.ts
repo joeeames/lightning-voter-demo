d65259245515284dadae03679b48d8f71f8fe3a1
Joe Eames <joeeames@gmail.com>
2017-05-13 13:50:42 -0600

    updated to latest typescript & typings

--- typings/main.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference path="main/ambient/angular-mocks/index.d.ts" />
-/// <reference path="main/ambient/angular-route/index.d.ts" />
-/// <reference path="main/ambient/angular/index.d.ts" />
-/// <reference path="main/ambient/jasmine/index.d.ts" />
-/// <reference path="main/ambient/jquery/index.d.ts" />
