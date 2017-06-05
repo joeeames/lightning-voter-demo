dee32cd4ce5fdb3969e413f70859138689eff3a6
Joe Eames <joeeames@gmail.com>
2017-06-05 13:52:02 -0600

    got aot build working

--- public/main-jit.ts
+++ public/main-jit.ts
@@ -1,16 +1,16 @@
+import { platformBrowserDynamic }    from '@angular/platform-browser-dynamic';
 import { UpgradeModule } from '@angular/upgrade/static';
 import { downgradeInjectable, downgradeComponent } from '@angular/upgrade/static';
 // this is done to make sure that typescript knows about all the rxjs operations
 import './app/rxjsOperations';
 
-import { AppModuleNgFactory } from '../aot/public/app/app.module.ngfactory'
+import { AppModule } from './app/app.module';
 import { NameParser } from "./app/admin/nameParser.service";
 import { UnreviewedTalkComponent } from "./app/home/unreviewedTalk.component";
-import { platformBrowser } from "@angular/platform-browser";
 
 declare var angular: angular.IAngularStatic;
 
-platformBrowser().bootstrapModuleFactory(AppModuleNgFactory).then(platformRef => {
+platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
   // upgrades & downgrades
   angular.module('app')
     .factory('nameParser', downgradeInjectable(NameParser))
