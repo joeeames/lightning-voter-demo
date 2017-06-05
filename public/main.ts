9cf59057dcee64cc96e67d5594bdbf3949bf7947
Joe Eames <joeeames@gmail.com>
2017-06-05 09:24:26 -0600

    migrated nameParser to ng2

--- public/main.ts
+++ public/main.ts
@@ -5,11 +5,14 @@ import { downgradeInjectable, downgradeComponent } from '@angular/upgrade/static
 import './app/rxjsOperations';
 
 import { AppModule } from './app/app.module';
+import { NameParser } from "./app/admin/nameParser.service";
 
 declare var angular: angular.IAngularStatic;
 
 platformBrowserDynamic().bootstrapModule(AppModule).then(platformRef => {
   // upgrades & downgrades
+  angular.module('app')
+    .factory('nameParser', downgradeInjectable(NameParser))
 
   const upgrade = platformRef.injector.get(UpgradeModule) as UpgradeModule;
   upgrade.bootstrap(document.documentElement, ['app']);
