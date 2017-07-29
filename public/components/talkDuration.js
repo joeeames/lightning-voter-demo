3aad55502adfa72bd04827c11a6c0ee4a4783d53
Joe Eames <joeeames@gmail.com>
2017-07-29 14:06:38 -0600

    removed unnecessary js files

--- public/components/talkDuration.js
+++
@@ -1,6 +0,0 @@
-angular.module('app').filter('talkDuration', function () {
-    return function (duration) {
-        return "Duration: " + duration + " minutes";
-    };
-});
-//# sourceMappingURL=talkDuration.js.map
